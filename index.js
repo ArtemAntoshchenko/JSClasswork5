acd6228212a41bee5152f0d8f683551042bb1779
Антощенко Артем Олегович <279017030@TOP.ACADEMY>
2025-03-26 20:59:29 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// function User(name,age) {
+//     return {
+//         name:name,
+//         age:age,
+//     }
+// }
+// let user = User('Имя',20)
+// alert(user.name)
+
+// let user={};
+// console.log(user.Random===undefined)
+
+// let user = {name:'daw',age:'123'};
+// alert('name' in user)
+
+// let user={}
+// for (let key in user) {
+//     alert(key) Вывидет весь объект
+//     alert(user[key]) Вывидет ключи
+// }
+
+// let schedule ={
+//     1:'daw'
+// }
+// function isEmpty(obj) {
+//     for(let key in obj) {
+//         return false;
+//     }
+//     return true
+// }
+// alert(isEmpty(schedule))
